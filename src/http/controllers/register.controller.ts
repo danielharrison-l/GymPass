fd454d8ff353269464004530e645bcacef646bc9
Daniel Harrison <d.harrison.lima@gmail.com>
2024-12-21 22:50:09 -0300

    feat: add factories and tests get-user-profile

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,5 +1,6 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma.users.repository";
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case";
 import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyRequest, FastifyReply } from "fastify";
 
@@ -17,8 +18,7 @@ export async function registerController(
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+    const registerUseCase = makeRegisterUseCase();
 
     await registerUseCase.execute({
       name,
