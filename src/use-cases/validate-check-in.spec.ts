2f4bcad950d9196644cd779ccc8f725501c4970c
Daniel Harrison <daniel.harrison.lima@gmail.com>
2024-12-29 22:40:35 -0300

    feat: late check-in validation

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -12,11 +12,11 @@ describe("Validate Check-in Use Case", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it("should be able to validate the check-in", async () => {
@@ -39,5 +39,24 @@ describe("Validate Check-in Use Case", () => {
         checkInId: "inexistent-check-in-id",
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of creation", async () => {
+    vi.setSystemTime(new Date(2022, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_Id: "gym-01",
+      user_Id: "user-01",
+    });
+
+    const twentyMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
