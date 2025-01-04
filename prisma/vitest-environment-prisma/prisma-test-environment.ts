fbe5fd963c648ff6e4a959991db00adfe7141512
Daniel Harrison <d.harrison.lima@gmail.com>
2025-01-04 19:06:13 -0300

    feat: add profile.spec controller

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,11 +1,40 @@
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
+import 'dotenv/config';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
+
 export default {
   name: "prisma",
+  transformMode: "ssr", 
   async setup() {
-    console.log("Prisma test environment setup");
+    const schema = randomUUID();
+    
+    const databaseURL = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync("npx prisma migrate deploy");
+
+
 
     return {
       async teardown() {
-        console.log("Teadown Prisma test environment");
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+        await prisma.$disconnect()
       },
     };
   },
